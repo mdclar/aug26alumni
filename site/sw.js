4a4ed452b17024332a4fa17e356bc09f609e734a
mdclar <marshall.clark@hotmail.com>
2026-07-30 21:42:24 -0600

    Add files via upload

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "jj-v39";
+const CACHE = "jj-v40";
 const ASSETS = [".", "index.html", "app.js", "manifest.json", "maps/region.jpg", "maps/oldcity.jpg", "icon-192.png", "icon-512.png"];
 self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
@@ -8,8 +8,7 @@ self.addEventListener("fetch", (e) => {
   // network-first: always prefer the freshest app; cache is the offline fallback
   e.respondWith(
     fetch(e.request).then((res) => {
-      const copy = res.clone();
-      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      if (res && res.ok) { const copy = res.clone(); caches.open(CACHE).then((c) => c.put(e.request, copy)); }
       return res;
     }).catch(() => caches.match(e.request).then((hit) => hit || caches.match("index.html")))
   );
